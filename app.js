d1dd2beabaca8d40d87834aa4660ef4e2dc8938f
hfz02 <muhammad.h@mail.ugm.ac.id>
2022-07-14 11:50:10 +0700

    commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.listen(3000, () => console.log('Server running at http://127.0.0.1:3000'))
+app.listen(5000, () => console.log('Server running at http://127.0.0.1:5000'))
 
 app.get('/', (req, res) => {
     res.sendFile('./views/index.html', { root: __dirname })
